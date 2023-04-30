883016c579289bc2aaade76b1f46dca6cc69ddda
David Leyva <dleyvaabrahantes@gmail.com>
2023-04-30 16:41:04 -0400

    Initial Commit

---
+++ ExpandibleButton/ExpandibleButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpandibleButtonApp.swift
+//  ExpandibleButton
+//
+//  Created by David on 4/30/23.
+//
+
+import SwiftUI
+
+@main
+struct ExpandibleButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
